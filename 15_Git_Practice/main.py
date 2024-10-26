ae66c8ea31a24e9e2770d7742c352fcc9e46aef8
Markos666 <markos1995.66@gmail.com>
2024-10-26 22:09:40 +0400

    added by Markos

--- 15_Git_Practice/main.py
+++ 15_Git_Practice/main.py
@@ -1,2 +1,11 @@
 print('Hello team!')
 print("Hello dear")
+import string
+
+alphabet = list(string.ascii_uppercase)
+# print(alphabet)
+print(alphabet[7], alphabet[4], alphabet[11], alphabet[11], alphabet[14])
+print(alphabet[4])
+print(alphabet[11])
+print(alphabet[11])
+print(alphabet[14])
