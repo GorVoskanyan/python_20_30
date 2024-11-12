8a455318827b81968b6a58a08d67f2b539391f81
Gor Voskanian <voskanyangor26@gmail.com>
2024-11-12 22:00:50 +0400

    added named directories

--- 22_File_Handling/os_info.py
+++ 22_File_Handling/os_info.py
@@ -1,9 +1,15 @@
 import sys
 import platform
+import os
 
-with open('os_info.txt', 'w', encoding='utf-8') as file:
+username = input('Enter your name: ')
+os.system(f'mkdir {username}')
+file_path = os.path.join(os.path.abspath('.'), username)
+file_name = 'os_info.txt'
+abs_path = os.path.join(file_path, file_name)
+
+with open(abs_path, 'w', encoding='utf-8') as file:
     file.write(
         f"Sys verison: {sys.version}\n"
         f"Platform: {platform.uname()}"
     )
-
